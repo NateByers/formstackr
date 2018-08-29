b263ed59639e9dd88e77077266d997d74420b6ec
Byers <nbyers1@iuhealth.org>
2018-08-29 10:37:05 -0400

    fix issue with getting submissions

--- R/get_submissions.R
+++ R/get_submissions.R
@@ -1,15 +1,9 @@
-# key <- Sys.getenv("FORMSTACK_KEY")
-# form_ids <- get_forms(key) %>%
-#   dplyr::filter(folder == "79048", submissions != "0") %>%
-#   dplyr::pull(form_id)
-
-
 
 
 #' @export
 get_submissions <- function(key, form_ids = NA_character_,
                             query_params = NA_character_) {
-  # query_params <- list(min_time = "2018-08-01")
+  # query_params <- list(min_time = "2018-08-25")
   if(is.na(form_ids[1])) {
     forms <- get_forms(key)
     form_ids <- forms$form_id
@@ -18,15 +12,20 @@ get_submissions <- function(key, form_ids = NA_character_,
   submission_ids <- lapply(form_ids, get_submission_ids, key = key,
                            query_params = query_params)
 
-  submission_ids <- submission_ids[!sapply(submission_ids, is.null)]
-
   submission_ids <- dplyr::bind_rows(submission_ids)
 
-  submission_data <- lapply(submission_ids$submission_id, get_submission_data, key = key) %>%
-    dplyr::bind_rows()
+  submissions <- apply(submission_ids %>%
+                         filter(type == ""), 1, function(row, key) {
+    print(row["id"])
+    print(row["type"])
+    get_submission_data(row["id"], row["type"], key) %>%
+      dplyr::mutate(form_id = row["form_id"])
+  }, key = key)
 
-  submissions <- submission_ids %>%
-    dplyr::full_join(submission_data, "submission_id")
+  submissions <- submissions %>%
+    dplyr::bind_rows() %>%
+    dplyr::distinct() %>%
+    dplyr::select(form_id, field_id, submission_id, value)
 
   submissions
 }
@@ -41,13 +40,21 @@ get_submission_ids <- function(form_id, key, query_params) {
                                                 key, query_params)
 
   submission_ids <- dplyr::data_frame(id = submission_ids_full,
-                                      partial = FALSE)
+                                      type = "")
 
   url_address_partial <- paste0("https://www.formstack.com/api/v2/form/",
                                 form_id, "/partialsubmission.json")
 
   submission_ids_partial <- extract_submission_ids(url_address_partial,
-                                                key, query_params)
+                                                   key, query_params)
+
+  submission_ids <- submission_ids %>%
+    dplyr::bind_rows(submission_ids,
+                     dplyr::data_frame(id = submission_ids_partial,
+                                       type = "partial")) %>%
+    dplyr::mutate(form_id = form_id)
+
+  submission_ids
 
 }
 
@@ -77,21 +84,21 @@ extract_submission_ids <-  function(url, key, query_params) {
 }
 
 
-get_submission_data <- function(submission_id, key, partial = FALSE) {
-  # submission_id <- submission_ids$submission_id[1]
-
-  if(partial) {
-    partial <- "partial"
-  } else {
-    partial <- ""
-  }
+get_submission_data <- function(submission_id, type, key) {
+  # submission_id <- "432899847"; type <- "partial"
 
   url_address <- paste0("https://www.formstack.com/api/v2/",
-                        partial, "submission/", submission_id, ".json")
+                        type, "submission/", submission_id, ".json")
 
   submission_data <- get_formstack_endpoint(url_address, key)
 
   submission_data <- httr::content(submission_data)$data
+
+  if(is.null(submission_data)) {
+    return(dplyr::data_frame(field_id = character(length = 0),
+                             value = character(length = 0),
+                             submission_id = character(length = 0)))
+  }
 
   submission_data <- lapply(submission_data, collapse_to_data_frame) %>%
     dplyr::bind_rows() %>%
